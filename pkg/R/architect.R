81d836c05f7243cb0d5eb3a9cc4801f13ca95155
Chionesu George <sapienti.Dei@outlook.com>
2023-03-23 17:14:53 -0400

    Updated Internal Functions
    Code updated to incorporate changes to DBOE as well as prepare for GA release

--- pkg/R/architect.R
+++ pkg/R/architect.R
@@ -1,12 +1,4 @@
-#' @title
-#' Intelligent Data Architecture
-#'
-#' @description
-#' \code{architect} is a \code{\link[data.table]{data.table}}-driven framework for intelligent data architecture.
-#' @name architect
-NULL
-
-define <- function(.data, ...){
+define <- function(data, ...){
 #' Define a Data Operation
 #'
 #' \code{define} allows one to operate on data using one or more formula-based definitions. Data transformation and selection can be achieved with formulas or using standard \code{\link[data.table]{data.table}} syntax in a procedural manner with a single function call.
@@ -14,7 +6,7 @@ define <- function(.data, ...){
 #' @details
 #' If \code{x} is a \code{\link[smart.data]{smart.data}} object, the taxonomical references to field names can be accessed by using the \code{use()} syntax in the right-hand side of the formula (e.g., \code{~use(term1, term2) + otherTerm1 + ...})
 #'
-#' @param x The input data (see 'Details')
+#' @param data The input data (see 'Details')
 #' @param ... (\code{\link[rlang]{dots_list}}): Formulae for which the left-hand side (LHS) is an expression containing the operation, and the right-hand side (RHS) contains column names that form a grouping set for the operation (i.e., \code{<expression> ~ col_1 + col_2 + ...}):
 #' \itemize{
 #' \item{If the form \code{<LHS>~ .} is given, the LHS executes using all columns as the grouping set}
@@ -24,69 +16,61 @@ define <- function(.data, ...){
 #'
 #' @return The data modified
 #' @export
-  force(.data);
+  force(data);
   .smartData <- fun_expr <- by_args <- NULL;
 
-	if ("smart.data" %in% installed.packages()[, "Package"]){
-		if (smart.data::is.smart(.data)){
-			.smartData <- .data$clone(deep = TRUE)
-			.data <- data.table::copy(.smartData$data)
+	if ("smart.data" %in% utils::installed.packages()[, "Package"]){
+		if (smart.data::is.smart(data)){
+			.smartData <- data$clone(deep = TRUE)
+			data <- data.table::copy(.smartData$data)
 		}
 	}
-	if (!data.table::is.data.table(.data)){ .data <- data.table::as.data.table(.data, keep.rownames = TRUE) }
+	if (!data.table::is.data.table(data)){ data <- data.table::as.data.table(data, keep.rownames = TRUE) }
 
-  .ops <- rlang::enexprs(..., .homonyms = "error", .check_assign = TRUE);
+  .ops <- rlang::enexprs(...);
 
   purrr::iwalk(.ops, ~{
-    env <- as.environment(.data);
-    nm	<- .y
-
     if (!rlang::is_formula(.x)){
       fun_expr <- .x;
       by_args <- character()
     } else {
-	    f 	<- eval(unlist(.x, recursive = FALSE)) |> data.table::setattr(".Environment", env);
+	    f <- eval(unlist(.x, recursive = FALSE)) |> data.table::setattr(".Environment", as.environment(data));
 
-      fun_expr <- rlang::f_lhs();
+      fun_expr <- rlang::f_lhs(f = f);
 
-      by_args <- purrr::map(attr(terms(f), "term.labels"), ~{
-					c(parse(text = glue::glue(".smartData${grep('use[()]', .x, value = TRUE)} |> names()"))
-						, glue::glue("{grep('use[()]', .x, value = TRUE, invert = TRUE)}")
-						) |>
-					purrr::compact() |>
-					sapply(eval, envir = environment())
-				}) |> unlist();
+      by_args <- stats::terms(f, data = data) |> attr("term.labels");
 
-      if (!all(by_args %in% names(.data))){ by_args <- character() }
+      if (!all(by_args %in% names(data))){ by_args <- character() }
     }
 
     .op <- if (!rlang::is_empty(fun_expr)){
 	    	# Operation Branch
-	    	.out_expr = if (.y != ""){
+	    	.out_expr <- if (.y != ""){
 		      # Assignment branch
-	          rlang::expr(.data[, `:=`(!!.y, !!fun_expr)])
+	          rlang::expr(data[, `:=`(!!.y, !!fun_expr)])
 	        } else {
 		      # Operation-only branch
-	          rlang::expr(.data[, !!fun_expr])
+	          rlang::expr(data[, !!fun_expr])
 	        }
 
+	    	# data.table grouping "by"
 	      if (!(rlang::is_empty(by_args) | identical(character(), by_args))){
 	      	.out_expr$by <- by_args
 	      }
 
-	    	# print(.out_expr)
+	    	.out_expr
 	    } else if (!rlang::is_empty(by_args)){
 	    	# Selection Branch
-	      rlang::expr(.data[, c(!!!by_args), with = FALSE])
+	      rlang::expr(data[, c(!!!by_args)])
 	    } else {
 	    	# Identity
-	    	.data
+	    	rlang::expr(data)
 	    }
 
-    .data <<- eval(.op);
+    data <<- eval(.op);
   });
 
-  return(.data)
+  return(data)
 }
 #
 join.mapper <- function(map_name = "new_join_map", env = parent.frame(), obj_names, field_names = "*", clean = FALSE){
@@ -102,6 +86,8 @@ join.mapper <- function(map_name = "new_join_map", env = parent.frame(), obj_nam
 #'
 #' @return If a \code{\link[DBOE]{DBOE}} DSN environment is passed to \code{env}, a local environment is used to contain the output of special code that transforms \code{env$metamap} into environment objects that can be used for creating the map: this should be stored or piped into \code{\link{join.reduce}}; otherwise, a \code{\link[data.table]{data.table}} object used join datasets via \code{\link{join.reduce}}
 #'
+#' @importFrom book.of.features logic_map
+#' @importFrom utils hasName
 #' @export
 
   # :: Argument Handling ----
@@ -109,121 +95,123 @@ join.mapper <- function(map_name = "new_join_map", env = parent.frame(), obj_nam
   map_name <- rlang::as_label(rlang::enexpr(map_name))
 
   # env: Special case of DBOE dsn environment
-  if (hasName(env, "metamap")){ setattr(env, "DBOE", TRUE) }
+  if (utils::hasName(env, "metamap")){ data.table::setattr(env, "DBOE", TRUE) }
 
   # field_names
-  field_names <- purrr::map_chr(field_names, ~{
-    if (any(stri_detect_fixed(deparse(.x), "=="))){
-      stringi::stri_replace_all_fixed(.x, " ", "", vectorize_all = FALSE)
-    } else {
-    	rlang::enexpr(!!.x) |> as.character()
-    }
-  }) |> unlist()
-	field_names <- purrr::as_mapper({
-		.this <- list()
-		.this$natural_joins <- .x[!grepl("[=]", .x)]
-		.this$equijoins <- .x[grepl("[=]", .x)]
-		.this
-	})(field_names)
+  fun <- purrr::as_mapper(~{ list(
+					natural_joins = .x[!grepl("[=$\\^()]", .x)] |> stringi::stri_split_regex(pattern = "[, |]", simplify = TRUE)
+					, equi_joins	= .x[grepl("[=]{2}", .x)]
+					, fuzzy_joins	= .x[grepl("[$\\^()]", .x)] |> stringi::stri_split_regex(pattern = "[, |]", simplify = TRUE)
+					)
+			});
+
+  field_names <- purrr::imap(fun(field_names), ~{
+  		vars <- stringi::stri_replace_all_fixed(.x, " ", "", vectorize_all = FALSE)
+			rlang::exprs(
+					natural_joins = intersect(names(.this), !!vars)
+          , equi_joins = purrr::keep(names(.this), ~grepl(!!vars, .x))
+          , fuzzy_joins = purrr::keep(names(.this), ~grepl(!!vars, .x))
+					)[[.y]]
+		})
 
   # :: Output ----
   if (clean){ rm(list = purrr::keep(map_name, exists, envir = env), envir = env) }
 
-  .queue <- mget(ls(pattern = paste(obj_names, collapse = "|"), envir = env), envir = env)
+  assign(map_name, {
+  	mget(ls(pattern = paste(obj_names, collapse = "|"), envir = env), envir = env) |>
+    purrr::imap_dfr(~{
+    	.this <- .x
+      obj_name <- .y
+      field_names <- purrr::imap(field_names, ~{ if (!rlang::is_empty(.x)){ eval(.x) }}) |>
+      								magrittr::freduce(list(purrr::compact, unlist, unique))
 
-  .output = {
-    purrr::imap_dfr(.queue, ~{
-      .this = .x;
-
-      data.table::data.table(
-        obj_name = .y
-        , field_names = map(field_names, ~{
-          .fn = .x;
-          ._1 = .fn[.fn %in% names(.this)]
-          ._2 = purrr::keep(names(.this), ~grepl(.fn, .x))
-          ._3 = if (any(purrr::map_lgl(names(.this), ~any(stringi::stri_detect_fixed(str = .fn, pattern = "==")) & grepl(.x, .fn)))){ .fn } else { NULL }
-
-          c(._1, ._2, ._3)
-        }) |> unlist() |> unique()
-      )
-    }) %>% {
-      .[!is.na(field_names)
-      ][, c(list(field_names = field_names), book.of.features::logic_map(.SD$obj_name))
-      ][, purrr::map(.SD, max), by = field_names
-      ][, unique(.SD)
-      ][, idx := order(.SD[, !"field_names"] |> apply(1, sum))
-      ] %T>% data.table::setkey(idx)
-    }
-  }
-
-  assign(map_name, data.table::setattr(.output, "env", env), envir = env)
-  if (.pipe_env){ return(env) }
+      data.table::data.table(obj_name, field_names)
+    }) |>
+		define(
+    	list(.SD[, .(field_names)], book.of.features::logic_map(obj_name))
+    	, purrr::map(.SD, max, na.rm = TRUE) ~ field_names
+    	, unique(.SD)
+    	, idx = order(.SD[, !"field_names"] |> apply(1, sum))
+    	) |>
+  		data.table::setkey(idx) |>
+  		data.table::setattr("env", env)
+    }, envir = env);
 }
 #
-join.reduce <- function(jmap, x.names, i.names, filters = TRUE, dt.key, env, clean = FALSE, .debug = FALSE){
+join.reduce <- function(join_map, out_name, x_names, i_names, filters = TRUE, dt_key, env = rlang::caller_env(), clean = FALSE){
 #' Join-Reduce Multiple Datasets
 #'
 #' \code{join.reduce} leverages \code{\link[data.table]{data.table}} fast joins and \code{\link[purrr]{reduce}} from package \code{purr}
 #'
-#' @param jmap The join-map (see \code{link{join.mapper}})
-#' @param x.names (string[]) Names or REGEX patterns indicating the outer table: multiple values will be concatenated into a delimited string
-#' @param i.names (string[]) Names or REGEX patterns indicating the inner table to be joined: multiple values will be concatenated into a delimited string
+#' @param join_map The input join-map (see \code{link{join.mapper}})
+#' @param out_name The name of the output object
+#' @param x_names (string[]) Names or REGEX patterns indicating the outer table: multiple values will be concatenated into a delimited string
+#' @param i_names (string[]) Names or REGEX patterns indicating the inner table to be joined: multiple values will be concatenated into a delimited string
 #' @param filters  (expression[[]]) \code{data.table}-friendly expression to limit the rows of the output. A length-2 list indicates \emph{row} and \emph{column} expressions respectively.
-#' @param dt.key (string[], symbol[]) Names that participate in creating the \code{\link[data.table]{key}} for the output
+#' @param dt_key (string[], symbol[]) Names that participate in creating the \code{\link[data.table]{key}} for the output
 #' @param env The environment in which the output should be assigned.  If empty, it defaults to \code{attr(jmap, "env")}
 #' @param clean (logical) \code{TRUE} indicates that the object should be removed from \code{env} before creating the output
-#' @param .debug (logical) Debugging flag: no output is produced
 #'
+#' @importFrom magrittr %>% %<>% %T>%
+#' @importFrom data.table %like% %ilike% .SD .N .GRP .I
 #' @return Combined datasets assigned to the designated environment
+#'
 #' @export
 
   # :: Argument Handling ----
-  # env
-  if (missing(env)){ env <- attr(jmap, "env") }
+  # x_names
+  if (!rlang::has_length(x_names, 1)){ x_names <- paste(x_names, collapse = "|") }
 
-  # x.names
-  if (!rlang::has_length(x.names, 1)){ x.names <- paste(x.names, collapse = "|") }
+  # i_names
+  if (rlang::is_empty(i_names)){
+    i_names <- paste(names(join_map) |> purrr::discard(~.x %ilike% x_names) |> c("field_names"), collapse = "|")
+  } else { if (!rlang::has_length(i_names, 1)){ i_names <- paste(i_names, collapse = "|") } else { i_names } }
 
-  # i.names
-  if (rlang::is_empty(i.names)){
-    i.names <- paste(names(jmap) %>% discard(~.x %ilike% x.names) %>% c("field_names"), collapse = "|")
-  } else { if (!rlang::has_length(i.names, 1)){ i.names <- paste(i.names, collapse = "|") } else { i.names } }
+  # join_map
+  if (!data.table::is.data.table(join_map)){ join_map %<>% data.table::as.data.table() }
 
-  # jmap
-  if (!data.table::is.data.table(jmap)){ jmap %<>% data.table::as.data.table() }
-  if (!"field_names" %in% names(jmap)){
+  if (!utils::hasName(join_map, "field_names")){
     stop("Column 'field_names' not found: exiting ...")
-  } else { data.table::setcolorder(jmap, "field_names") }
-  if (!all(jmap[, !c("field_names", "idx")] %>% unlist() %>% unique() %in% c(0, 1))){
-    stop("Join map contains values other than zero (0) or (1) after the first column: exiting ...")}
-  jmap %<>% .[pmap_lgl(jmap[, mget(ls(pattern = x.names))], function(...){ sum(c(...)) > 0 })];
+  } else {
+  	data.table::setcolorder(join_map, "field_names")
+  }
+  if (!all(join_map[, !c("field_names", "idx")] |> unlist() |> unique() %in% c(0, 1))){
+    stop("Join map contains values other than zero (0) or (1) after the first column: exiting ...")
+  }
+
+	# Remove rows from `join_map` with all zeroed entries
+	join_map <- join_map[purrr::pmap_lgl(join_map[, mget(ls(pattern = x_names))], function(...){ sum(c(...)) > 0 })];
 
   # filters
   if (!is.list(filters)){ filters <- as.list(filters) }
-  if (rlang::has_length(filters, 1)){ filters <- append(filters, list(quote(mget(ls())))) } else if (!rlang::has_length(2)){ filters <- filters[c(1:2)] }
-
-  # dt.key
-  if (!missing(dt.key)){ dt.key <- {
-    .tmp_key = substitute(dt.key) %>% as.character();
-
-    if (rlang::has_length(.tmp_key, 1)){ .tmp_key } else { .tmp_key[-1] }
+  if (rlang::has_length(filters, 1)){
+  	filters <- append(filters, list(quote(mget(ls()))))
+  } else if (!rlang::has_length(2)){
+  	filters <- filters[c(1:2)]
   }
-  } else { dt.key <- NULL}
+
+  # dt_key
+  if (!missing(dt_key)){
+  	dt_key <- {
+	    .tmp_key = as.character(substitute(dt_key));
+	    if (rlang::has_length(.tmp_key, 1)){ .tmp_key } else { .tmp_key[-1] }
+	  }
+  } else { dt_key <- NULL}
 
   # :: Definitions ----
-  xion.func = function(...){
+  xion.func <- function(...){
     # Creates a list that is 'data.table'-join-ready
     ._j = list(...);
-    ._i = ...names()[(._j == 1) & (grepl(i.names, ...names(), ignore.case = TRUE))];
+    ._i = ...names()[(._j == 1) & (grepl(i_names, ...names(), ignore.case = TRUE))];
     if (length(._i) == 0){ return(NULL) };
 
-    ._x = ...names()[(._j == 1) & (grepl(x.names, ...names(), ignore.case = TRUE))];
+    ._x = ...names()[(._j == 1) & (grepl(x_names, ...names(), ignore.case = TRUE))];
     ._on = ._j[[1]];
 
     list(x = c(._x), i = c(._i), on = c(._on)) |> expand.grid()
   }
 
-  join.func = function(cur, nxt){ sprintf(
+  join.func <- function(cur, nxt){ sprintf(
     fmt = "%s[%s, on = c(%s), allow.cartesian = TRUE]"
     , cur
     , nxt[1]
@@ -231,43 +219,41 @@ join.reduce <- function(jmap, x.names, i.names, filters = TRUE, dt.key, env, cle
 	  )
   }
 
-  .alpha = { purrr::pmap(jmap, xion.func) |>
+  .alpha <- { purrr::pmap(join_map, xion.func) |>
       purrr::compact() |>
-      data.table::rbindlist() %>%
-      .[!(on %ilike% "source"), .(on = list(I(on))), by = .(x, i)]
+      data.table::rbindlist()
+  .alpha[!(on %ilike% "source"), .(on = list(I(on))), by = .(x, i)]
   }
 
-  .beta = { .alpha[
-	    , list(rlang::new_box(.SD[, .(i, on)] |> apply(1, I))), by = x][
-	    , purrr::imap(set_names(V1, x), ~{
+  .beta <- { .alpha[
+	    , list(rlang::new_box(.SD[, .(i, on)] |> apply(1, I))), by = x
+	    ][
+	    , purrr::imap(purrr::set_names(V1, x), ~{
 			    .this = .y; .that = .x;
 			    purrr::reduce(.x = .that, .f = join.func, .init = .this)
 				 })
 	    ]}
 
-  if (.debug){ print(list(alpha = .alpha, beta = .beta)) }
-
   # :: Output ----
   .output = purrr::map(.beta, ~{
-    rfilter = filters[[1]]
+    row_filter = filters[[1]]
 
-    cfilter = filters[[2]]
+    colfilter = filters[[2]]
 
-    .out = eval(str2lang(.x), envir = attr(jmap, "env"))
+    .out = eval(str2lang(.x), envir = attr(join_map, "env"))
 
     .out %<>% {
-    	.[eval(rfilter), eval(cfilter)][, mget(discard(ls(), ~.x %ilike% "strptime|^i[.]"))] %>%
-        unique() %>%
+    	.[eval(row_filter), eval(col_filter)][, mget(discard(ls(), ~.x %ilike% "strptime|^i[.]"))] %>%
+        unique() |>
         setnames("full_dt", "event_date", skip_absent = TRUE)
     }
 
-    if (rlang::is_empty(dt.key)){ .out } else { data.table::setkeyv(.out, dt.key) |> data.table::setcolorder() }
-
+    if (rlang::is_empty(dt_key)){ .out } else { data.table::setkeyv(.out, dt_key) |> data.table::setcolorder() }
   }) %>%
   	purrr::set_names(paste0("J_data_", stringi::stri_pad_left(seq_along(.), width = stringi::stri_length(as.character(length(.))))))
 
   if (clean){ rm(list = names(.output), envir = env) }
 
-  list2env(.output, envir = env)
+  assign(out_name, .output, envir = env)
 }
 
